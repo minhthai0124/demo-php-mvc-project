00dd1b366f623f974c326ddccb5d6b354631c38b
minhthai0124 <minhthai0124@gmail.com>
2017-10-05 15:30:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
 
-// require 'vendor/autoload.php';
+require 'vendor/autoload.php';
 
-// require 'core/boostrap.php';
+require 'core/boostrap.php';
 
-// // or use App\Core\{Route, Request};
-// use App\Core\Route;
-// use App\Core\Request;
+// or use App\Core\{Route, Request};
+use App\Core\Route;
+use App\Core\Request;
 
-// Route::load('app/routes.php')
-//     ->direct(Request::uri(), Request::method());
-
-echo "hello";
+Route::load('app/routes.php')
+    ->direct(Request::uri(), Request::method());
