b915dfa033259564a0cf78f6c70c84de034218ed
minhthai <minhthai0124@gmail.com>
2017-10-05 15:07:16 +0700

    Test to deploy project in heroku

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
-require 'vendor/autoload.php';
+// require 'vendor/autoload.php';
 
-require 'core/boostrap.php';
+// require 'core/boostrap.php';
 
-// or use App\Core\{Route, Request};
-use App\Core\Route;
-use App\Core\Request;
+// // or use App\Core\{Route, Request};
+// use App\Core\Route;
+// use App\Core\Request;
 
-Route::load('app/routes.php')
-    ->direct(Request::uri(), Request::method());
+// Route::load('app/routes.php')
+//     ->direct(Request::uri(), Request::method());
+
+echo "hello";
